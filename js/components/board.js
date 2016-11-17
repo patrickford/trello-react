27cf8a3c3d6e600fe62d40de81fe16190e1e7e14
Brian Barrow <briancbarrow@gmail.com>
2016-11-17 05:57:17 -0700

    Finished Communicating between Components

--- js/components/board.js
+++ js/components/board.js
@@ -2,12 +2,19 @@ var React = require('react');
 var List = require('./list');
 
 var Board = React.createClass({
+    onAddSubmit(event) {
+        event.preventDefault();
+        console.log('Submitted');
+    },
+    onAddInputChanged() {
+        console.log('Changed');
+    },
     render: function(props) {
         return (
             <div className="board">{this.props.title}
-                <List title="First"/>
-                <List title="Second"/>
-                <List title="Third"/>
+                <List onChange={this.onAddInputChanged} onSubmit={this.onAddSubmit} title="First"/>
+                <List onChange={this.onAddInputChanged} onSubmit={this.onAddSubmit} title="Second"/>
+                <List onChange={this.onAddInputChanged} onSubmit={this.onAddSubmit} title="Third"/>
             </div>
             )
     }
--- js/components/list.js
+++ js/components/list.js
@@ -2,13 +2,23 @@ var React = require('react');
 var Card = require('./card');
 
 var List = React.createClass({
+    // handleSubmit(event) {
+        
+    //     props.onAddSubmit();
+    // },
+    
     render: function(props) {
         return (
             <div className="list">{this.props.title}
                 <Card text="This is some text"/>
                 <Card text="This is second text"/>
                 <Card text="This is third text"/>
+                <form className="form" onSubmit={this.props.onSubmit}>
+                    <input type="text" name="input" onChange={this.props.onChange}/>
+                    <input type="submit" value="Submit"/>
+                </form>
             </div>
+            
             )
     }
 });
